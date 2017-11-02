918c8661e9e92f1d675d53a8956b57d58880ef48
alevens1 <alevesn1@icloud.com>
2017-11-02 13:43:59 -0400

    database configuration, heroku deployment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import User from "../models/user"
+
+User.create({
+    firstName: "Kamala",
+    lastName: "King",
+    email: 'k.king@gmail.com',
+    password: "******"
+  }, function(err, doc){});
 
 ReactDOM.render(<App />, document.getElementById("root"));
